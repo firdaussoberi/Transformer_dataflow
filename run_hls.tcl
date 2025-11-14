5b28b7a454abd500a237ea9fb0716558c196735c
Firdaus Soberi <firdaussoberi@gmail.com>
2025-11-14 17:54:27 +0000

    Update file inclusion with conditional cflags
    Refactor file inclusion to use conditional compilation flags based on linear_mode.

--- run_hls.tcl
+++ run_hls.tcl
@@ -1,7 +1,19 @@
 open_project -reset Transformer_accel
 set_top accel
-add_files Accel.cpp -cflags "-ILayers"
-add_files -tb TestBenchAccel.cpp -cflags "-ITests -Wno-unknown-pragmas"
+
+# 0 = BitLinear
+# 1 = Linear
+set linear_mode 0
+if {$linear_mode == 1} {
+    set cdefs "-D LINEAR"
+} else {
+    set cdefs "-D BITLINEAR"
+}
+add_files Accel.cpp -cflags "-ILayers $cdefs"
+add_files -tb TestBenchAccel.cpp -cflags "-ITests $cdefs -Wno-unknown-pragmas"
+#add_files Accel.cpp -cflags "-ILayers"
+#add_files -tb TestBenchAccel.cpp -cflags "-ITests -Wno-unknown-pragmas"
+
 open_solution -reset "solution1" -flow_target vivado
 set_part {xck24-ubva530-2LV-c}
 create_clock -period 5 -name default
